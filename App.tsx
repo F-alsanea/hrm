75d816021118d29411d739ffa4f863dffb724e7a
F-alsanea <Falsanea@aol.com>
2026-02-07 05:56:38 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -248,17 +248,13 @@ const App: React.FC = () => {
           background-size: 1.1rem;
           text-align: center;
           cursor: pointer;
-          padding-top: 0.8rem;
-          padding-bottom: 0.8rem;
-          padding-inline-start: 3.5rem;
-          padding-inline-end: 1.5rem;
+          padding: 0.8rem 3.5rem !important;
+          line-height: 1.5;
           background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='black' stroke-opacity='0.4' stroke-width='3'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='M19 9l-7 7-7-7'%3E%3C/path%3E%3C/svg%3E");
           background-position: left 1.2rem center;
         }
 
         [dir="ltr"] select {
-          padding-inline-start: 3.5rem;
-          padding-inline-end: 1.5rem;
           background-position: right 1.2rem center;
         }
 
